31b1b8c1f5bc99b9cd5502856a92a0f7b3a82ab6
Lucizka <83407389+Lucizka@users.noreply.github.com>
2022-11-23 13:49:42 +0100

    new update

--- src/app/components/FlashCards/flashcard-menu/flashcard-menu.component.ts
+++ src/app/components/FlashCards/flashcard-menu/flashcard-menu.component.ts
@@ -12,4 +12,28 @@ export class FlashcardMenuComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  testTest: testSubjet[] =
+    [
+      {name: "Variables", description: "This is a test", exercises: ["Integer", "Strings", "Booleans"], id: 1,completion: 80,downloaded: false},
+      {name: "Test 3", description: "This is a test", exercises: ["Exercise 1", "Exercise 2", "Exercise 3"], id: 1,completion: 10,downloaded: true},
+      {name: "Test 4", description: "This is a test", exercises: ["Exercise 1", "Exercise 2", "Exercise 3"], id: 1,completion: 10,downloaded: true},
+      {name: "Test 5", description: "This is a test", exercises: ["Exercise 1", "Exercise 2", "Exercise 3"], id: 1,completion: 10,downloaded: true},
+      {name: "Test 6", description: "This is a test", exercises: ["Exercise 1", "Exercise 2", "Exercise 3"], id: 1,completion: 10,downloaded: true},
+      {name: "Test 7", description: "This is a test", exercises: ["Exercise 1", "Exercise 2", "Exercise 3"], id: 1,completion: 10,downloaded: true},
+    ]
+
+
 }
+
+
+class testSubjet {
+  name!: string;
+  description!: string;
+  exercises!: string[];
+  id!: number
+  completion!: number;
+  downloaded!: boolean;
+}
+
+
+
