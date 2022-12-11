6e9116f08cf7bff176c97a3544914b89fc01f208
Lucizka <zenerluda@gmail.com>
2022-12-11 15:15:40 +0100

    added Flash card functionality to http://localhost:4200/Flashcard
    flashcards added , if its repeating to load just wait

--- src/app/components/FlashCards/flashcard/flashcard.component.ts
+++ src/app/components/FlashCards/flashcard/flashcard.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlashcardComponent implements OnInit {
 
+
+  isFlipped = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
